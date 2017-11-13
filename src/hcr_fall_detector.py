6a86623cadcae7ec53acb2869330a5ff0ed8fb50
BaronKhan <bksonic@hotmail.co.uk>
2017-11-13 19:52:06 +0000

    hcr_fall_detection.py: add dummy code

--- src/hcr_fall_detector.py
+++ src/hcr_fall_detector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy, time, sys, signal
-from std_msgs.msg import Int8, Float32MultiArray
+from std_msgs.msg import Int8, Float32MultiArray, Float64MultiArray
+from random import randrange
 
 STATE_NORMAL = 0
 STATE_FALLING = 1
@@ -25,12 +26,18 @@ imu_pos = []
 rospy.Subscriber("imu_pos", Float32MultiArray, imu_callback)
 
 gait_raw = []
-rospy.Subscriber("gait_raw", Float32MultiArray, gait_callback)
+rospy.Subscriber("gait_raw", Float64MultiArray, gait_callback)
 
 falling_state = STATE_NORMAL
 pub_falling_state = rospy.Publisher('falling_state', Int8, queue_size=10)
 
 if __name__ == '__main__':
   while True:
+    # Begin dummy code
+    if (randrange(10) < 2):
+      falling_state = (falling_state + 1) % 3
+      pub_falling_state.publish(falling_state)
+    # End dummy code
+
     # Sleep to avoid consuming all the CPU at once
     time.sleep(0.1)
