aaee8de597df2c5f5830c1100bf740242107dd40
BaronKhan <bksonic@hotmail.co.uk>
2017-11-13 18:21:56 +0000

    Update hcr_fall_detector.py

--- src/hcr_fall_detector.py
+++ src/hcr_fall_detector.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python
 import rospy, time, sys, signal
-from std_msgs.msg import Bool, String
+from std_msgs.msg import Int8, Float32MultiArray
+
+STATE_NORMAL = 0
+STATE_FALLING = 1
+STATE_FALLEN = 2
 
 def kill_process(signal, frame):
-  print("\nExiting fall_detection...")
+  print("Exiting hcr_fall_detector...")
   sys.exit(0)
+
+def imu_callback():
+  pass
+
+def gait_callback():
+  pass
 
 # Kill process with Ctrl+C
 signal.signal(signal.SIGINT, kill_process)
 
-rospy.init_node('fall_detection', anonymous=True)
-pub_has_fallen = rospy.Publisher('has_fallen', Bool, queue_size=10)
-has_fallen = False
+rospy.init_node('hcr_fall_detector', anonymous=True)
 
-pub_is_falling = rospy.Publisher('is_falling', Bool, queue_size=10)
-is_falling = False
+imu_pos = []
+rospy.Subscriber("imu_pos", Float32MultiArray, imu_callback)
 
-def HasFallen():
-  pass
+gait_raw = []
+rospy.Subscriber("gait_raw", Float32MultiArray, gait_callback)
 
-def AboutToFall():
-  pass
+falling_state = STATE_NORMAL
+pub_falling_state = rospy.Publisher('falling_state', Int8, queue_size=10)
 
 if __name__ == '__main__':
   while True:
     # Sleep to avoid consuming all the CPU at once
-    time.sleep(0.2)
+    time.sleep(0.1)
